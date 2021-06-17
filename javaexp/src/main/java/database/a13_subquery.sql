a8eab368c5fa442c6097fca0a56cac8f2eebbc95
Realysk <ysk2388@gmail.com>
2021-06-17 14:49:44 +0900

    06.17 14:49PM

--- javaexp/src/main/java/database/a13_subquery.sql
+++ javaexp/src/main/java/database/a13_subquery.sql
@@ -39,3 +39,89 @@ WHERE to_char(hiredate, 'MM') = (
 	SELECT to_char(hiredate, 'MM')
 	FROM emp
 	WHERE ename = 'JAMES');
+	
+/*
+ # 서브쿼리의 종류
+ 	1. 단일 행 서버 쿼리
+ 		- 서버 쿼리의 결과 값이 단일 데이터로 조건에서 사용할 때 활용된다.
+ 		- 비교연산자( = , > , < , >= , <= , != )등 활용할 수 있다.
+ 
+ 	2. 다중 행 서버 쿼리
+ 		- 서버 쿼리의 결과 값이 다중으로 조건에서 사용할 때 활용된다.
+ 		- 다중 행 비교연산자 : in, any, some, all, exists 등을 활용할 수 있다.
+ */
+
+-- 단일 행 서버 쿼리
+-- 부서 10에서 가장 급여가 높은 사원의 정보를 출력하세요.
+SELECT max(sal)
+FROM emp
+WHERE deptno = 10;
+SELECT *
+FROM emp
+WHERE sal = (
+	SELECT max(sal)
+	FROM emp
+	WHERE deptno = 10);
+	
+-- 부서 20에서 평균 급여보다 높은 사원의 정보를 출력하세요.
+SELECT *
+FROM emp
+WHERE sal < (
+	SELECT avg(sal)
+	FROM emp
+	WHERE deptno = 20);
+	
+SELECT * FROM emp;
+-- ex1) 부서번호 20에서 가장 적은 연봉보다 같거나 적은 사원들을 출력하세요.
+SELECT *
+FROM emp
+WHERE sal > (
+	SELECT min(sal)
+	FROM emp
+	WHERE deptno = 20);
+
+-- ex2) 2사분기에 입사한 사원들의 평균 급여보다 많은 사원들을 출력하세요.
+SELECT *
+FROM emp
+WHERE sal > (
+	SELECT avg(sal)
+	FROM emp
+	WHERE to_char(hiredate,'Q') = 2);
+
+-- ex3) comm이 가장 많은 사원의 정보를 출력하세요.
+SELECT *
+FROM emp
+WHERE comm = (
+	SELECT max(comm)
+	FROM emp);
+
+-- 다중 행 subquery
+-- 연봉이 1000~3000 사이의 직책에 해당하는 사원을 출력
+SELECT * FROM emp;
+
+SELECT *
+FROM emp
+WHERE job IN (
+	SELECT DISTINCT job
+	FROM emp
+	WHERE sal BETWEEN 1000 AND 2000);
+
+SELECT *
+FROM emp;
+
+
+SELECT *
+FROM emp
+WHERE (sal, deptno) IN (
+	SELECT sal, deptno FROM emp WHERE sal >= 5000);
+
+-- 부서별로 연봉이 가장 높은 사원 정보 출력
+SELECT *
+FROM emp
+WHERE (deptno, sal) IN (
+	SELECT deptno, max(sal)
+	FROM emp
+	GROUP BY deptno);
+
+-- ex1) 4~6월에 입사한 사원의 직책과 같은 사람들을 출력하세요.
+-- ex2) 연봉이 2000~3000 사이의 사원정보의 부서번호화 같은 사람들을 출력하세요.
