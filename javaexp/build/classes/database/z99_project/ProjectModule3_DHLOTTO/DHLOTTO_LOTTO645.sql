67d88dfa08300265a2106199f9cd636dccdc5f91
Realysk <ysk2388@gmail.com>
2021-06-19 19:13:32 +0900

    06.19 19:13PM

--- javaexp/build/classes/database/z99_project/ProjectModule3_DHLOTTO/DHLOTTO_LOTTO645.sql
+++ javaexp/build/classes/database/z99_project/ProjectModule3_DHLOTTO/DHLOTTO_LOTTO645.sql
@@ -17,3 +17,9 @@ INSERT INTO LOTTO645 VALUES ('961','20210501','36, 9, 10, 25, 42, 14','12');
 INSERT INTO LOTTO645 VALUES ('960','20210424','45, 4, 10, 21, 44, 15','23');
 SELECT date1 "회차", date2 "날짜", myLotto "복권번호", myLotto_bn "보너스번호"
 FROM LOTTO645;
+
+
+-- 복권 입력 여부 검색 (검색 내용 : '961'회 복권 입력 정보 또는 복권번호 '25'가 들어간 복권 입력 정보)
+SELECT date1 "회차", date2 "날짜", myLotto "복권번호", myLotto_bn "보너스번호"
+FROM LOTTO645
+WHERE date1 LIKE '%961%' OR myLotto LIKE '%25%';
