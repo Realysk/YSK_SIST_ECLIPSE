1efe3fcf5cfed7b78d94a1cd967fa4c49bb3fe11
Realysk <ysk2388@gmail.com>
2021-06-27 12:57:26 +0900

    06.27 12:57PM

--- javaexp/src/main/java/database/z99_project/ProjectModule4_JSTUDY/JSTUDY_COMMUNITY.sql
+++ javaexp/src/main/java/database/z99_project/ProjectModule4_JSTUDY/JSTUDY_COMMUNITY.sql
@@ -1,5 +1,4 @@
 -- 커뮤니티 메뉴에서 각 게시판 별로 입력 한 게시물 정보를 테이블에 도출
--- (연관관계 필수 - join)
 
 -- JSTUDY 커뮤니티 : 자유 게시판
 
@@ -8,7 +7,7 @@ CREATE TABLE JSCOM_FREE (
 	writeTitle varchar2(300), -- 게시물 제목
 	writer varchar2(100), -- 게시물 작성자
 	writeDate date, -- 게시물 작성일자
-	writeCont varchar(300) -- 게시물 내용
+	writeCont varchar2(300) -- 게시물 내용
 );
 
 INSERT INTO JSCOM_FREE VALUES (6, '반갑습니다!', '홍시', sysdate ,'홈페이지 제작 처음입니다!');
@@ -28,10 +27,13 @@ CREATE TABLE JSCOM_STUDY (
 	writeTitle varchar2(300), -- 게시물 제목
 	writer varchar2(100), -- 게시물 작성자
 	writeDate date, -- 게시물 작성일자
-	writeCont varchar(300) -- 게시물 내용
+	writeCont varchar2(300) -- 게시물 내용
 );
 
-INSERT INTO JSCOM_STUDY VALUES (3, 'HTML 대망의 첫 강의', 'hihihi', sysdate, '파고들면 쉽습니다!');
+INSERT INTO JSCOM_STUDY VALUES (6, 'SPRING이란?', '봄이왔네요', sysdate, '봄입니다...');
+INSERT INTO JSCOM_STUDY VALUES (5, 'JSP 1강입니다', 'jsp일타강사', TO_DATE('2021-06-27 01:07:46','YYYY-MM-DD HH:MI:SS'), 'JSP 1강입니다');
+INSERT INTO JSCOM_STUDY VALUES (4, 'CSS란 무엇인가 -1-', '유길동', TO_DATE('2021-06-26 11:11:11','YYYY-MM-DD HH:MI:SS'), 'HTML과 함께하는 CSS');
+INSERT INTO JSCOM_STUDY VALUES (3, 'HTML 대망의 첫 강의', 'hihihi', TO_DATE('2021-06-26 09:22:12','YYYY-MM-DD HH:MI:SS'), '파고들면 쉽습니다!');
 INSERT INTO JSCOM_STUDY VALUES (2, '오라클 설정방법', 'DB를누벼라', TO_DATE('2021-06-26 09:22:12','YYYY-MM-DD HH:MI:SS'), '깔끔하게 정리해봤습니다.');
 INSERT INTO JSCOM_STUDY VALUES (1, 'JAVA 첫 시작! 1강입니다.', '자바를잡으러', TO_DATE('2021-06-25 08:12:55','YYYY-MM-DD HH:MI:SS'), '첫 강의영상입니다! 도움이 되셨으면 좋겠네요~');
 
@@ -45,31 +47,39 @@ CREATE TABLE JSCOM_QNA (
 	writeTitle varchar2(300), -- 게시물 제목
 	writer varchar2(100), -- 게시물 작성자
 	writeDate date, -- 게시물 작성일자
-	writeCont varchar(300) -- 게시물 내용
+	writeCont varchar2(300) -- 게시물 내용
 );
 
-INSERT INTO JSCOM_QNA VALUES (3, 'DB 질문이요', '어려운취준생', sysdate, '이클립스랑 오라클 연결 어떻게 해요?');
+INSERT INTO JSCOM_QNA VALUES (6, '호환성 질문이요', '쉽지않네요', sysdate, '맥이랑 호환성 좋나요?');
+INSERT INTO JSCOM_QNA VALUES (5, '질문', '마길자', TO_DATE('2021-06-27 04:42:27','YYYY-MM-DD HH:MI:SS'), '안녕하세요~');
+INSERT INTO JSCOM_QNA VALUES (4, 'api의 뜻이 뭐죠?', '현직삼성개발자', TO_DATE('2021-06-26 10:23:25','YYYY-MM-DD HH:MI:SS'), 'API가 뭔지 아직 잘 모르겠어요');
+INSERT INTO JSCOM_QNA VALUES (3, 'DB 질문이요', '어려운취준생', TO_DATE('2021-06-26 09:22:12','YYYY-MM-DD HH:MI:SS'), '이클립스랑 오라클 연결 어떻게 해요?');
 INSERT INTO JSCOM_QNA VALUES (2, '자바 관련 질문입니다.', 'byeman', TO_DATE('2021-06-25 06:08:45','YYYY-MM-DD HH:MI:SS'), 'C언어랑 연관있나요?');
 INSERT INTO JSCOM_QNA VALUES (1, '뭐하는 사이트에요?', '여긴어디지', TO_DATE('2021-06-25 12:16:25','YYYY-MM-DD HH:MI:SS'), '사이트 주제를 모르겠어요..');
 
 SELECT writeNum "No", writeTitle "제목", writer "작성자", writeDate "작성 일자", writeCont "내용"
 FROM JSCOM_QNA;
 
+-- JSTUDY 커뮤니티 : 검색 내역
 
+CREATE TABLE JSCOM_SEARCH (
+	jsLoc varchar2(300), -- 주요 키워드
+	writeNum number -- 검색 지역
+);
 
--- SQL
--- ex1) 부서명, 사원명을 join으로 출력하세요.
-SELECT dname, ename -- 출력할 데이터 컬럼
-FROM emp e, dept d -- 테이블명
-WHERE e.deptno=d.deptno; -- join관계에 있는 컬럼을 지정
+INSERT INTO JSCOM_SEARCH VALUES ('서울 중랑구', 1);
+INSERT INTO JSCOM_SEARCH VALUES ('서울 강남구', 5);
+INSERT INTO JSCOM_SEARCH VALUES ('서울 종로구', 3);
+INSERT INTO JSCOM_SEARCH VALUES ('서울 도봉구', 2);
+INSERT INTO JSCOM_SEARCH VALUES ('서울 강북구', 4);
+INSERT INTO JSCOM_SEARCH VALUES ('서울 노원구', 6);
 
--- ex1) 급여가 1000~3000사이의 부서명, 사원명, 급여를 출력하세요.
-SELECT dname, ename, sal
-FROM emp e, dept d
-WHERE e.deptno = d.deptno
-AND sal BETWEEN 1000 AND 3000;
+SELECT jsLoc "검색 지역", writeNum "게시물 번호"
+FROM JSCOM_SEARCH;
 
-SELECT writeNum, writeTitle, writer, jf.JSCOM_FREE
-FROM JSCOM_FREE jf, JSCOM_STUDY js
-WHERE jf.JSCOM_FREE=js.JSCOM_STUDY
-AND writeNum BETWEEN 1 AND 3;
+-- SQL (연관 관계 필수 - join)
+-- 작성자가 'byeman'인 사용자의 게시물 정보 출력
+SELECT *
+FROM JSCOM_FREE jf, JSCOM_SEARCH js
+WHERE jf.writeNum=js.writeNum
+AND writer = 'byeman';
