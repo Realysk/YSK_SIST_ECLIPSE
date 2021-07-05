d16c13a6f227ba75d69005d3e42cb6382a9fb442
user <user@DESKTOP-COJ9858>
2021-07-05 18:25:56 +0900

    07.05 18:25PM (DB 연동 성공)

---
+++ javaexp/src/main/java/database/a99_dbconn/dbTesc.java
@@ -0,0 +1,35 @@
+package database.a99_dbconn;
+
+import java.sql.*;
+
+public class dbTesc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			 
+			//2] drivermanager 클래스의 getConnection() 메소드로 오라클에 연결 시도 
+			String url = "jdbc:oracle:thin:@110.93.182.83:1521:xe";     
+        	String user = "system";       
+        	String password = "1111";
+        	Connection conn = DriverManager.getConnection(url, user, password);  //외부자원 예외 발생 
+        	
+        	System.out.println("데이터베이스 연결 성공");
+        
+
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+
+
+	}
+
+}
