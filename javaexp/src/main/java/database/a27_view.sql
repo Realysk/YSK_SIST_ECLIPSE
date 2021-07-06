46b0aa34c41c03ff892a0f8c91fef838855c025e
user <user@DESKTOP-COJ9858>
2021-07-06 15:49:41 +0900

    07.06 15:50PM

--- javaexp/src/main/java/database/a27_view.sql
+++ javaexp/src/main/java/database/a27_view.sql
@@ -13,6 +13,13 @@
  		1) 단순 뷰 : 하나의 기본 테이블에 의해 정의 된 뷰
  		2) 복합 뷰 : 두 개 이상의 기본 테이블로 구성한 뷰
  			여러 테이블의 조인 관계를 통한 결과를 처리한 뷰
+ 		# 인라인 뷰 (inline view)
+ 			- from절에 참조하는 테이블의 크기가 클 경우 필요한 행과 컬럼만으로 구성된 집합을 재정의하여 질의문을 효율적으로 구성하는 것을 말한다.
+ 			- from 절에서 서버쿼리를 사용하여 생성한 임시 뷰를 말한다.
+ 			- SQL 명령문이 실행되는 동안만 임시적으로 정의한다.
+ 			select 컬럼
+			from (inline view)
+ 			where 조건
  	4. 뷰의 한계
  		1) 제한적 사용 : 무결성 제약조건, 표현식, group by 유무에 따라 DML명령문은 제한적으로 사용된다.
  		2) 수행 불가능 : distinct , 그룹함수, group by, start with, connect by, rownum은 포함할 수 없다.
@@ -35,3 +42,39 @@ SELECT * FROM v_emp;
 -- view는 실제적으로 물리적으로 테이블이 만들어지는 것이 아니기에 기존 emp에 영향을 미치거다 받는다.
 
 -- ex) 부서별 인원, 최대급여, 최소급여를 뷰로 (v_emp_sum) 만들어 조회하세요.
+CREATE VIEW v_emp_sum
+AS SELECT count(*) cnt, max(sal) maxsal, min(sal) minsal
+FROM emp
+GROUP BY deptno;
+SELECT * FROM v_emp_sum;
+
+-- 단순 view를 통한 dml 처리
+DROP TABLE emp31;
+CREATE TABLE emp31
+AS SELECT * FROM emp;
+CREATE VIEW view_emp31
+AS SELECT empno, ename, sal, deptno
+FROM emp31
+WHERE deptno = 10;
+SELECT * FROM view_emp31;
+-- view에 DML 처리
+INSERT INTO view_emp31 VALUES (7999,'ANGEL',7000,10);
+SELECT * FROM emp31;
+
+-- 복합 뷰 만들기
+CREATE TABLE dept04
+AS SELECT * FROM dept;
+CREATE VIEW view_emp_dept
+AS SELECT e.empno, e.ename, e.sal, e.deptno, d.dname, d.loc
+FROM emp31 e, dept04 d
+WHERE e.deptno = d.deptno
+ORDER BY empno DESC;
+SELECT * FROM view_emp_dept;
+-- 복합 뷰의 데이터 입력
+INSERT INTO view_emp_dept VALUES (8000,'HIMAN',3000,20,'인사','서울');
+-- cannot modify a column which maps to a non key-preserved table
+-- 복합 뷰는 에러가 발생한다.
+
+-- 뷰의 삭제
+-- drop view 뷰이름
+DROP VIEW view_emp_dept;
