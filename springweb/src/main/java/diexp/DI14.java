c99abab67997a40d2c09aa5191a000054d57ad7a
Realysk <ysk2388@gmail.com>
2021-09-09 14:08:02 +0900

    09.09 14:07PM

--- springweb/src/main/java/diexp/DI14.java
+++ springweb/src/main/java/diexp/DI14.java
@@ -13,12 +13,16 @@ public class DI14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// 스프링의 컨테이너의 메모리 내용 확인
 		Resource rs = new ClassPathResource("diexp\\a14.xml");
 		BeanFactory bean = new XmlBeanFactory(rs);
+		// id로 해당 bean의 객체를 가져온다.
 		Dept dept = (Dept)bean.getBean("dept");
 		System.out.println("부서번호 : " + dept.getDeptno());
 		System.out.println("부서명 : " + dept.getDname());
 		System.out.println("부서지역 : " + dept.getLoc());
+		
+		// ex) DI15.java, a15.xml을 만들고, Student.java를 활용하여 생성자를 통해 데이터를 할당하고 출력하세요.
 
 	}
 }
