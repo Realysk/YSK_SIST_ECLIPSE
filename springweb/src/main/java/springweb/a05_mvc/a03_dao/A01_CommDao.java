22494511fb5a97e1d4f60523a3dc0ef72692f170
Realysk <ysk2388@gmail.com>
2021-09-13 11:52:45 +0900

    09.13 11:52AM

--- springweb/src/main/java/springweb/a05_mvc/a03_dao/A01_CommDao.java
+++ springweb/src/main/java/springweb/a05_mvc/a03_dao/A01_CommDao.java
@@ -5,7 +5,7 @@ public class A01_CommDao {
 	public interface A01_CommDao {
 		public ArrayList<Emp> empList();
 		// 실제 상속받는 객체는 framework에서 만들어 줌
-		// mybatis frame(wml로 설정된 내용)
+		// mybatis frame(wml로 설정된 내용).
 	}
 
 }
